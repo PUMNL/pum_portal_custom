35dec5a5978f7f9c6df0ab0d9fb634301c30a71a
Hans Idink <hidink@orgis.nl>
2015-06-16 13:38:47 +0200

    private community required issue #2607

--- pum_portal_webform_help/ppwh.js
+++ pum_portal_webform_help/ppwh.js
@@ -7,10 +7,15 @@
         var p = $(this).parent().parent().children("label");
         $(this).html("<span class='helpbutton'>&nbsp</span><div>"+$(this).text()+"</div>");
         $(this).appendTo(p);
+        $(this).find("div").append("<span class='close'>Close</span>");
       });
      
       $('code.help span.helpbutton', context).click(function() {
           $(this).parent().find("div").toggle();  
+      });
+      
+      $('code.help span.close').click(function(){
+          $(this).parent().hide();
       });
 
     }
